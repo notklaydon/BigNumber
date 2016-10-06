2032eff61645530c6af417fd7b726239ee568a80
Klaydon Balicanta <phlippy.klaydon@gmail.com>
2016-10-06 16:48:18 -0400

    Made formatting changes.

--- BigNumber.java
+++ BigNumber.java
@@ -7,8 +7,7 @@ package bignumber;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class BigNumber
-{
+public class BigNumber {
     int[] bigArray;
     //ArrayList<Integer> bigArray = new ArrayList<Integer>();
     //int sign = 0;     //sign variable to indicate positivity/negativity for
@@ -29,12 +28,11 @@ public class BigNumber
     * with the 0th value representing the sign value of a 10's complement
     * number
     */
-    public BigNumber(String input)
-    {
-        // the following commented out lines of code are meant to serve as
-        // the assignment of the sign for a sign and magnitude BigNumber
-        // and are commented out for the current iteration of the BigNumber
-        // class as it is currently being represented as 10s complement
+    public BigNumber(String input) {
+        /*The following commented out lines were meant to serve as
+        the assignment of the sign for a sign and magnitude BigNumber
+        and are commented out for the current iteration of the BigNumber
+        class as it is currently being represented as 10s complement.*/
         
         /*if (input.startsWith("-", 0))
         {
@@ -43,8 +41,7 @@ public class BigNumber
             sign = 0;
         }*/
         bigArray = new int[input.length()];
-        for(int i = 0; i < input.length(); i++) 
-        {
+        for(int i = 0; i < input.length(); i++) {
             bigArray[i] = Character.getNumericValue(input.charAt(i));
         }
     }
@@ -53,13 +50,12 @@ public class BigNumber
     *   negate - 10's complement negation
     *   @return BigNumber object that is negated in 10's complement
     */
-    public BigNumber negate() //public void negate()
-    {
+    public BigNumber negate() {//public void negate()
+
         BigNumber negated;
         String temp = "";
         int i = 0;
-        while(i < (bigArray.length - 1))
-        {
+        while(i < (bigArray.length - 1)) {
             temp += (9 - this.bigArray[i]);
             i++;
         }
@@ -69,10 +65,9 @@ public class BigNumber
     
     /*
     /**
-    *   negate - sign negation of a sign and magnitude BigNumber
+    *   negate - sign negation for sign and magnitude BigNumber representation
     * /
-    public void negate()
-    {
+    public void negate() {
         if(this.sign = 0)
             this.sign = 1;
         else 
@@ -86,11 +81,9 @@ public class BigNumber
     *   @return String representation of BigNumber object
     */
     @Override
-    public String toString()
-    {
+    public String toString() {
         String result = "";
-        for(int i = 0; i < this.bigArray.length; i++)
-        {
+        for(int i = 0; i < this.bigArray.length; i++) {
             result += this.bigArray[i];
         }
         return result;
@@ -101,25 +94,19 @@ public class BigNumber
     * input BigNumber
     *   @return boolean value of equality check
     */
-    public boolean Equals(BigNumber bn)
-    {
+    public boolean Equals(BigNumber bn) {
         this.normalize();
         bn.normalize();
         
         if((bn.sign() != this.sign()) || (bn.bigArray.length != this.bigArray.length))
-        {
             return false;
-        }
-        else 
-        {
+        else {
             for(int i = 0; i < this.bigArray.length; i++) {
-                if(this.bigArray[i] != bn.bigArray[i]) {
+                if(this.bigArray[i] != bn.bigArray[i])
                     return false;
-                }
             }
             return true;
         }
-        
     }
 
    /**
@@ -130,18 +117,17 @@ public class BigNumber
     * the specified object.
     *
     */
-    public int compareTo(BigNumber bn)
-    {
+    public int compareTo(BigNumber bn) {
         if(this.sign() < bn.sign()) {
-            return 1;   //will return positive integer if compared BigNumber
-                        //is greater than this BigNumber Object
+            return 1;   /*will return positive integer if compared BigNumber
+                        is greater than this BigNumber Object*/
         } else if (this.sign() > bn.sign()) {
-            return -1;  //will return negative integer if compared BigNumber
-                        //is lesser than this BigNumber object
-        } else {
-        return 0;   //will return 0 if compared BigNumber and this BigNumber
-                    //object are equal sign
+            return -1;  /*will return negative integer if compared BigNumber
+                        is lesser than this BigNumber object*/
         }
+        else
+            return 0;   /*will return 0 if compared BigNumber and this BigNumber
+                        object are equal sign*/
     }
 
 
@@ -158,7 +144,7 @@ public class BigNumber
             return 1;   // negative
     }
     
-    /**
+    /* *
     *   sign - returns the value of a BigNumber as a 0 for positive of 1
     * for negative
     * sign and magnitude
@@ -171,13 +157,10 @@ public class BigNumber
    /**
     * normalize - removes any unnecessary leading sign representations
     */
-    public void normalize()
-    {
+    public void normalize() {
         int indexHalt = this.bigArray[0];
             while(bigArray[indexHalt] == bigArray[indexHalt + 1])
-             {
-                indexHalt += 1;
-            }
+            {indexHalt += 1;}
             this.bigArray = Arrays.copyOfRange(bigArray, indexHalt, bigArray.length - 1);
     }
     
@@ -191,8 +174,7 @@ public class BigNumber
      */
     private int[] reversify(int[] toBeReversed) {
         int[] reversed = new int[toBeReversed.length];
-        for(int i = 0; i < toBeReversed.length; i++)
-        {
+        for(int i = 0; i < toBeReversed.length; i++) {
             reversed[i] = toBeReversed[toBeReversed.length - 1];
         }
         return reversed;
